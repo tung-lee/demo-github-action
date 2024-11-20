4e23d6a56918b9b22556094a6d2d6f5611bab4c5
Le Khac Thanh Tung <87592549+tung-lee@users.noreply.github.com>
2024-11-20 15:07:12 +0700

    Update index.router.js

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -8,4 +8,11 @@ router.get('/checkstatus', (req, res, next) => {
     })
 })
 
+router.get('/test-github-workflow', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'ci/cd working'
+    })
+})
+
 module.exports = router;
